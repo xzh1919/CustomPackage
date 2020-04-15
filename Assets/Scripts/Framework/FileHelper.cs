0de25ee48b5097e20008fff6235e65ec76b3f176
Jeffy <xzh0709@163.com>
2020-04-15 17:38:37 +0800

    first

---
+++ Assets/Scripts/Framework/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileHelper
+{
+    public static string[] GetFiles(string path, string pattern = "*.*", SearchOption option = SearchOption.AllDirectories)
+    {
+        if (!Directory.Exists(path))
+        {
+            return new string[] { };
+        }
+
+        if (string.IsNullOrEmpty(pattern))
+        {
+            pattern = "*.*";
+        }
+
+        return Directory.GetFiles(path, pattern, option);
+    }
+}
